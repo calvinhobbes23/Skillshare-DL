259a12848b7ebecf12ceb1146b84056d31c4a7f2
Andrea <80418102+Th3h4k3r@users.noreply.github.com>
2023-03-08 13:53:44 +0100

    Update dl.py

--- dl.py
+++ dl.py
@@ -5,7 +5,7 @@ from skillshare import Skillshare, splash
 # dl.download_course_by_class_id(189505397)
 
 def main():
-    dl = Skillshare("PHPSESSID=YOUR_PHPSESSID_HERE")
+    dl = Skillshare("PHPSESSID=1c6bfc1748fab7e814da01145c00be0c")
     course_url = sys.argv[1]
     #dl.download_course_by_url(course_url)
     dl.download_course_by_class_id(course_url)
